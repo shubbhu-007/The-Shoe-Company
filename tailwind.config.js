ef7e7a470b3df15112bb015f69132116ff77771a
Shubham Sonkar <shubhamsonkar5657@gmail.com>
2023-04-19 15:00:15 -0400

    different media queries added for different screen to make responsive (a little bit finishing left)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens:{
+        "media418" : "418px"
+      },
+    },
   },
   plugins: [],
 }
