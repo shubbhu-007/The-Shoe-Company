99e91bf5800ba76ebb6e6e87b12b80ed81d7060d
Shubham Sonkar <shubhamsonkar5657@gmail.com>
2023-04-21 15:37:06 -0400

    testimonials , contact section added and completion of responsive site has been done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       screens:{
         "media418" : "418px"
       },
+      backgroundImage:{
+        'Testimonial-background' : "url('./images/photo.png')",
+      }
     },
   },
   plugins: [],
